5498813317d753623061847c34045fd80b1b6303
Lilian Gimenez <lilian.gimenez@laposte.net>
2024-03-24 19:14:17 +0100

    Substitutions array

--- src/keyboard_labels.nim
+++ src/keyboard_labels.nim
@@ -1,8 +1,8 @@
-import std/[json, jsonutils, options, strformat, strutils, tables, unicode, xmlparser, xmltree]
+import std/[json, jsonutils, options, sequtils, strformat, strutils, tables, unicode, xmlparser, xmltree]
 import pixie
 
 type
-  Legend = object
+  LegendPlace = object
     # JSON data
     fontPaths: seq[string]
     size: float
@@ -24,13 +24,13 @@ type
     uses: int = 0
     typeface: Typeface
 
-  Substitution = object
+  LegendItem = object
     string: string
     translate = vec2()
     scale = vec2(1)
     image: Image
 
-template findChild[T](node:XmlNode, child:untyped, elementTag:string, attrName:string, attrValue:T, success: untyped,
+template findChild[T](node:XmlNode; child:untyped; elementTag:string; attrName:string; attrValue:T; success: untyped;
     failure: untyped) =
   block loop:
     for `child` {.inject.} in node:
@@ -46,13 +46,14 @@ template findChild[T](node:XmlNode, child:untyped, elementTag:string, attrName:s
     failure
 
 var ppcm: float
+var typefaces: Table[string, TypefaceData]
 
 func getColor(node: JsonNode): ColorRGBA =
   rgba node["r"].getInt.uint8, node["g"].getInt.uint8, node["b"].getInt.uint8, 255
 
 proc getPixels(x: JsonNode): float = ppcm * x.getFloat
 
-proc getLegend(node: JsonNode, base: Option[Legend] = none(Legend)): Legend =
+proc getLegendPlace(node: JsonNode; base: Option[LegendPlace] = none(LegendPlace)): LegendPlace =
   if base.isSome : result = base.unsafeGet
   if node.contains "fonts":
     result.fontPaths.setLen 0
@@ -68,6 +69,53 @@ proc getLegend(node: JsonNode, base: Option[Legend] = none(Legend)): Legend =
   if node.contains "posX": result.pos.x = node["posX"].getPixels
   if node.contains "posY": result.pos.y = node["posY"].getPixels
   if node.contains "align": result.align.fromJson node["align"]
+
+proc getSubstitution(node: JsonNode): LegendItem =
+  # result is not intitialized with default values, I don't kynow why
+  result.string = ""
+  result.translate = vec2()
+  result.scale = vec2(1)
+  result.image = nil
+
+  if node.contains "string": result.string = node["string"].getStr
+  if node.contains "image": result.image = readImage node["image"].getStr
+  if node.contains "translateX": result.translate.x = node["translateX"].getPixels
+  if node.contains "translateY": result.translate.y = node["translateY"].getPixels
+  if node.contains "scaleX": result.scale.x = node["scaleX"].getFloat
+  if node.contains "scaleY": result.scale.y = node["scaleY"].getFloat
+  if node.contains "scale": result.scale = vec2 node["scale"].getFloat
+
+proc renderLegend(image: Image; place: LegendPlace; item: LegendItem; color: Color; posX, posY: float) =
+  if item.image == nil:
+    block fontsLoop:
+      for font in place.fonts:
+        var hasGlyphs = true
+        block runesLoop:
+          for rune in item.string.runes:
+            if not font.typeface.hasGlyph rune:
+              hasGlyphs = false
+              break runesLoop
+        if hasGlyphs:
+          var transform = translate(vec2(posX, posY) + place.pos + item.translate) * scale(item.scale)
+          font.paint.color = color
+          image.fillText font, item.string, transform, hAlign = place.align
+          typefaces[font.typeface.filePath].uses += 1
+          break fontsLoop
+      echo &"Glyphs for {item.string} not found"
+  else:
+    let extraTranslate = (if place.align == RightAlign: -ppcm * item.image.width.float * item.scale.x /
+        item.image.height.float else: 0)
+    var transform = translate(vec2(posX + extratranslate, posY) + place.pos + item.translate) *
+        scale(item.scale * ppcm / item.image.height.float)
+    var newImage = item.image.copy()
+    var transformColor = mat3(color.r, color.g, color.b,
+        place.otherColor.r, place.otherColor.g, place.otherColor.b, 0, 0, 0)
+    for pixel in newImage.data.mitems:
+      var v = transformColor * vec3(pixel.r.float, pixel.g.float, pixel.b.float)
+      pixel.r = v.x.uint8
+      pixel.g = v.y.uint8
+      pixel.b = v.z.uint8
+    image.draw newImage, transform
 
 proc main() =
   echo "Reading data"
@@ -88,43 +136,35 @@ proc main() =
   let padding = keysNode["padding"].getPixels
   let codesArray = keysNode["codes"]
 
-  var typefaces = initTable[string, TypefaceData]()
-  let baseLegends = keysNode["baseLegends"].getLegend
-  var legends = newSeq[Legend]()
-  for legend in keysNode["legends"]: legends.add legend.getLegend some(baseLegends)
-  for legend in legends.mitems:
-    for fontPath in legend.fontPaths:
+  typefaces = initTable[string, TypefaceData]()
+  let baseLegendPlace = keysNode["baseLegends"].getLegendPlace
+  var legendPlaces = newSeq[LegendPlace]()
+  for legendPlace in keysNode["legends"]: legendPlaces.add legendPlace.getLegendPlace some(baseLegendPlace)
+  for legendPlace in legendPlaces.mitems:
+    for fontPath in legendPlace.fontPaths:
       var font = newFont (if typefaces.contains fontPath: typefaces[fontPath].typeface
       else:
         let typeface = readTypeface fontPath
         typefaces[fontPath] = TypefaceData(typeface: typeface)
         typeface)
-      font.size = legend.size
-      legend.fonts.add font
+      font.size = legendPlace.size
+      legendPlace.fonts.add font
     block findKeyMaps:
-      var mapSetName = legend.keyMapSet
-      var keyMapIndex = legend.keyMapIndex
+      var mapSetName = legendPlace.keyMapSet
+      var keyMapIndex = legendPlace.keyMapIndex
       while true:
         findChild keyLayout, keyMapSet, "keyMapSet", "id", mapSetName:
           findChild keyMapSet, keyMap, "keyMap", "index", keyMapIndex:
-            legend.keyMaps.add keyMap
+            legendPlace.keyMaps.add keyMap
             mapSetName = keyMap.attr("baseMapSet")
             if mapSetName == "": break findKeyMaps
             keyMapIndex = keyMap.attr("baseIndex").parseInt
           do: quit &"keyMap {keyMapIndex} in keyMapSet {mapSetName} not found"
         do: quit &"keyMapSet {mapSetName} not found"
 
-  var substitutions = initTable[string, Substitution]()
+  var substitutions = initTable[string, seq[LegendItem]]()
   for key, node in settingsJson["substitutions"]:
-    var substitution = Substitution()
-    if node.contains "string": substitution.string = node["string"].getStr
-    if node.contains "image": substitution.image = readImage node["image"].getStr
-    if node.contains "translateX": substitution.translate.x = node["translateX"].getPixels
-    if node.contains "translateY": substitution.translate.y = node["translateY"].getPixels
-    if node.contains "scaleX": substitution.scale.x = node["scaleX"].getFloat
-    if node.contains "scaleY": substitution.scale.y = node["scaleY"].getFloat
-    if node.contains "scale": substitution.scale = vec2 node["scale"].getFloat
-    substitutions[key] = substitution
+    substitutions[key] = if node.kind == JArray: node.mapIt it.getSubstitution else: @[node.getSubstitution]
 
   let actions = keyLayout.child("actions")
 
@@ -142,61 +182,33 @@ proc main() =
     path.rect(posX, posY, keyWidth, keyHeight)
     image.fillPath path, keyBackground
     let keyCode = code.getInt
-    for legend in legends:
+    for legendPlace in legendPlaces:
       block findKeyMaps:
-        for keyMap in legend.keyMaps:
+        for keyMap in legendPlace.keyMaps:
           findChild keyMap, keyElement, "key", "code", keyCode:
             let actionName = keyElement.attr("action")
             findChild actions, action, "action", "id", actionName:
-              findChild action, state, "when", "state", legend.stateName:
+              findChild action, state, "when", "state", legendPlace.stateName:
                 let nextState = state.attr("next")
-                var str: string
+                var legendItem: LegendItem
                 var color: Color
                 if nextState == "":
-                  str = state.attr("output")
-                  color = legend.color
+                  legendItem.string = state.attr("output")
+                  color = legendPlace.color
                 else:
-                  str = "dead_" & nextState
-                  color = legend.deadKeyColor
-                var strTranslate = vec2()
-                var strScale = vec2(1)
-                var labelImage: Image
-                if substitutions.contains str:
-                  let substitution = substitutions[str]
-                  if substitution.string != "": str = substitution.string
-                  strTranslate = substitution.translate
-                  strScale = substitution.scale
-                  labelImage = substitution.image
-                if labelImage == nil:
-                  block fontsLoop:
-                    for font in legend.fonts:
-                      var hasGlyphs = true
-                      block runesLoop:
-                        for rune in str.runes:
-                          if not font.typeface.hasGlyph rune:
-                            hasGlyphs = false
-                            break runesLoop
-                      if hasGlyphs:
-                        var transform = translate(vec2(posX, posY) + legend.pos + strTranslate) * scale(strScale)
-                        font.paint.color = color
-                        image.fillText font, str, transform, hAlign = legend.align
-                        typefaces[font.typeface.filePath].uses += 1
-                        break fontsLoop
-                    echo &"Glyphs for {str} not found"
+                  legendItem.string = "dead_" & nextState
+                  color = legendPlace.deadKeyColor
+                # it should be intitialized but it's not
+                legendItem.translate = vec2()
+                legendItem.scale = vec2(1)
+                legendItem.image = nil
+                if substitutions.contains legendItem.string:
+                  for substitution in substitutions[legendItem.string]:
+                    var overridenLegend = substitution
+                    if substitution.string == "": overridenLegend.string = legendItem.string
+                    image.renderLegend legendPlace, overridenLegend, color, posX, posY
                 else:
-                  let extraTranslate = (if legend.align == RightAlign: -ppcm * labelImage.width.float * strScale.x /
-                      labelImage.height.float else: 0)
-                  var transform = translate(vec2(posX + extratranslate, posY) + legend.pos + strTranslate) *
-                      scale(strScale * ppcm / labelImage.height.float)
-                  var newImage = labelImage.copy()
-                  var transformColor = mat3(color.r, color.g, color.b,
-                      legend.otherColor.r, legend.otherColor.g, legend.otherColor.b, 0, 0, 0)
-                  for pixel in newImage.data.mitems:
-                    var v = transformColor * vec3(pixel.r.float, pixel.g.float, pixel.b.float)
-                    pixel.r = v.x.uint8
-                    pixel.g = v.y.uint8
-                    pixel.b = v.z.uint8
-                  image.draw newImage, transform
+                  image.renderLegend legendPlace, legendItem, color, posX, posY
               do: discard
             do: echo "Action ", actionName, " not found"
             {.push warning[UnreachableCode]:off.}
